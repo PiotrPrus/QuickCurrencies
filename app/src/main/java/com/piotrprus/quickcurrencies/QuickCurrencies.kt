435899952c7663ef8d5b52bc9b9d6ea313ae1a60
PiotrPrus <prus.piotr@gmail.com>
2019-08-20 22:52:50 +0200

    add missing reflect dependency, configure koin

---
+++ app/src/main/java/com/piotrprus/quickcurrencies/QuickCurrencies.kt
@@ -0,0 +1,30 @@
+package com.piotrprus.quickcurrencies
+
+import android.app.Application
+import com.piotrprus.quickcurrencies.common.di.viewModelModule
+import io.reactivex.plugins.RxJavaPlugins
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import timber.log.Timber
+
+class QuickCurrencies : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@QuickCurrencies)
+            modules(
+                listOf(
+                    viewModelModule
+                )
+            )
+        }
+        RxJavaPlugins.setErrorHandler {
+            Timber.e(it, "The error not be delivered to callback")
+        }
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
---
+++ app/src/main/java/com/piotrprus/quickcurrencies/common/di/ViewModelModule.kt
@@ -0,0 +1,9 @@
+package com.piotrprus.quickcurrencies.common.di
+
+import com.piotrprus.quickcurrencies.feature.main.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+    viewModel { MainViewModel() }
+}
