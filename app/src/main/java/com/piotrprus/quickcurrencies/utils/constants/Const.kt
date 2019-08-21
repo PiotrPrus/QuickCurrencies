d6f565b24b8b314ad76a8dffa3849e12b0319f17
PiotrPrus <prus.piotr@gmail.com>
2019-08-22 00:29:58 +0200

    create const list that store reference to full currency name and flag drawable

--- app/src/main/java/com/piotrprus/quickcurrencies/utils/constants/Const.kt
+++ app/src/main/java/com/piotrprus/quickcurrencies/utils/constants/Const.kt
@@ -1,6 +1,43 @@
 package com.piotrprus.quickcurrencies.utils.constants
 
+import com.piotrprus.quickcurrencies.R
+
 object Const {
     const val EUR_CODE: String = "EUR"
     const val REVOLUT_BASE_URL = "https://revolut.duckdns.org"
+    val CURRENCY_LIST = mapOf(
+        "eur" to (R.string.eur_full_name to R.drawable.eur_flag),
+        "aud" to (R.string.aud_full_name to R.drawable.aud_flag),
+        "bgn" to (R.string.bgn_full_name to R.drawable.bgn_flag),
+        "brl" to (R.string.brl_full_name to R.drawable.brl_flag),
+        "cad" to (R.string.cad_full_name to R.drawable.cad_flag),
+        "chf" to (R.string.chf_full_name to R.drawable.chf_flag),
+        "cny" to (R.string.cny_full_name to R.drawable.cny_flag),
+        "czk" to (R.string.czk_full_name to R.drawable.czk_flag),
+        "dkk" to (R.string.dkk_full_name to R.drawable.dkk_flag),
+        "gbp" to (R.string.gbp_full_name to R.drawable.gbp_flag),
+        "hkd" to (R.string.hkd_full_name to R.drawable.hkd_flag),
+        "hrk" to (R.string.hrk_full_name to R.drawable.hrk_flag),
+        "huf" to (R.string.huf_full_name to R.drawable.huf_flag),
+        "idr" to (R.string.idr_full_name to R.drawable.idr_flag),
+        "ils" to (R.string.ils_full_name to R.drawable.ils_flag),
+        "inr" to (R.string.inr_full_name to R.drawable.inr_flag),
+        "isk" to (R.string.isk_full_name to R.drawable.isk_flag),
+        "jpy" to (R.string.jpy_full_name to R.drawable.jpy_flag),
+        "krw" to (R.string.krw_full_name to R.drawable.krw_flag),
+        "mxn" to (R.string.mxn_full_name to R.drawable.mxn_flag),
+        "myr" to (R.string.myr_full_name to R.drawable.myr_flag),
+        "nok" to (R.string.nok_full_name to R.drawable.nok_flag),
+        "nzd" to (R.string.nzd_full_name to R.drawable.nzd_flag),
+        "php" to (R.string.php_full_name to R.drawable.php_flag),
+        "pln" to (R.string.pln_full_name to R.drawable.pln_flag),
+        "ron" to (R.string.ron_full_name to R.drawable.ron_flag),
+        "rub" to (R.string.rub_full_name to R.drawable.rub_flag),
+        "sek" to (R.string.sek_full_name to R.drawable.sek_flag),
+        "sgd" to (R.string.sgd_full_name to R.drawable.sgd_flag),
+        "thb" to (R.string.thb_full_name to R.drawable.thb_flag),
+        "try" to (R.string.try_full_name to R.drawable.try_flag),
+        "usd" to (R.string.usd_full_name to R.drawable.usd_flag),
+        "zar" to (R.string.zar_full_name to R.drawable.zar_flag)
+    )
 }
